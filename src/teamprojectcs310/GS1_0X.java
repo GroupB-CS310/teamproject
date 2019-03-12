80f463101f9944199c6e370aa05a29bf75e38804
Panda <khalebgroce@gmail.com>
2019-03-12 15:42:25 -0500

    DOne with classes 0 and 2

--- src/teamprojectcs310/GS1_0X.java
+++ src/teamprojectcs310/GS1_0X.java
@@ -11,7 +11,24 @@ public class GS1_0X extends GS1 {
         
         super.matchers = new HashMap<>();
         
+        super.matchers.put( "00", Pattern.compile("^00[0-9]{18}") ); // AI #00
         super.matchers.put( "01", Pattern.compile("^01[0-9]{14}") ); // AI #01
+        super.matchers.put( "02", Pattern.compile("^02[0-9]{14}") ); // AI #02
+        
+    }
+    
+        public Object parse00(String element) {
+        
+        HashMap<String, Object> output = new HashMap<>();
+        System.out.println("Found Element String: " + element);
+        
+        output.put("title", "SSCC");
+        output.put("ai", element.substring(0, 2) );
+        output.put("extension", Integer.parseInt( element.substring(3) ) );
+        output.put("check", Integer.parseInt( element.substring(19) ) );
+        output.put("element", element);
+        
+        return output;
         
     }
     
@@ -21,6 +38,22 @@ public class GS1_0X extends GS1 {
         System.out.println("Found Element String: " + element);
         
         output.put("title", "GTIN");
+        output.put("ai", element.substring(0, 2) );
+        output.put("prefix", Integer.parseInt( element.substring(2, 10) ) );
+        output.put("item", Integer.parseInt( element.substring(10, 15) ) );
+        output.put("check", Integer.parseInt( element.substring(15) ) );
+        output.put("element", element);
+        
+        return output;
+        
+    }
+    
+        public Object parse02(String element) {
+        
+        HashMap<String, Object> output = new HashMap<>();
+        System.out.println("Found Element String: " + element);
+        
+        output.put("title", "CONTENT");
         output.put("ai", element.substring(0, 2) );
         output.put("prefix", Integer.parseInt( element.substring(2, 10) ) );
         output.put("item", Integer.parseInt( element.substring(10, 15) ) );
--- src/teamprojectcs310/Main.java
+++ src/teamprojectcs310/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         
         PlinkoMasterParser p = new PlinkoMasterParser();
-        String s = p.parse("11052497");
+        String s = p.parse("255071234892367721415289");
     }
 }
