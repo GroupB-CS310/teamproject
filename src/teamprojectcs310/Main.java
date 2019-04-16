431569ba5e1a0166a543f3bb487ea39466bb8890
Bryson Leonard <bleonard1@stu.jsu.edu>
2019-04-16 17:44:35 -0500

    This version works perfectly. I have a test case in main that has many AI's from different classes and they all parsed great. Give it a try

--- src/teamprojectcs310/Main.java
+++ src/teamprojectcs310/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         
         PlinkoMasterParser p = new PlinkoMasterParser();
-        String s = p.parse("255123456789012345685");
+        String s = p.parse("901234567890%911234567890%921234567890%931234567890%4001234567890%333n123456");
     }
 }
